ecb8d85e44f77e263af531ce1f4b444390196100
ayush <ius.maharjan@gmail.com>
2013-04-21 09:57:59 +0545

    data added

--- db.php
+++ db.php
@@ -3,11 +3,13 @@
 require_once('includes/init.inc.php');
 
 $year_start = 1971;
-$year_end = 2008;
-$table = "energy_production";
+$year_end = 2007;
+$table = "transmission_loss";
 
 
-for($y=1971; $y<=2008; $y++){
+for($y=1971; $y<=2007; $y++){
 	$sql = "update {$table} set `{$y}`=NULL where `{$y}`=0";
 	SQL_to_JS_var::find_by_sql($sql);
 }
+
+echo "successful";
